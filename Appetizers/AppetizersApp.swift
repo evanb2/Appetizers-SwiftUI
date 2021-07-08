d1906b43250675b29458ac48870f7b8d21407abb
Evan Butler <evanb447@gmail.com>
2021-07-08 16:08:25 -0700

    init project

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Evan Butler on 7/8/21.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
