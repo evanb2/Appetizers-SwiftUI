abf66d1a47ad25ea9e03371dd705a960646ac7f6
Evan Butler <evanb447@gmail.com>
2021-07-09 11:02:22 -0700

    fetch Appetizers in list view

--- Appetizers/AppetizerListView.swift
+++ Appetizers/AppetizerListView.swift
@@ -8,12 +8,32 @@
 import SwiftUI
 
 struct AppetizerListView: View {
+    @State private var appetizers: [Appetizer] = []
+    
     var body: some View {
         NavigationView {
-            List(MockData.appetizers) { appetizer in
+            List(appetizers) { appetizer in
                 AppetizerListCell(appetizer: appetizer)
             }
             .navigationTitle("🍟 Appetizers")
+        }
+        .onAppear {
+            fetchAppetizers()
+        }
+    }
+    
+    func fetchAppetizers() {
+        NetworkManager.shared.getAppetizers { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let appetizers):
+                    // because in SwiftUI updating the data will update the UI
+                    // this needs to happen on the main thread
+                    self.appetizers = appetizers
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
         }
     }
 }
