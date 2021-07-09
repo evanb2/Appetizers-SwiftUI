504d1f6e835eb29aa06162fe157190c5ccb7c99f
Evan Butler <evanb447@gmail.com>
2021-07-09 15:40:50 -0700

    create RemoteImage view

--- Appetizers/AppetizerListCell.swift
+++ Appetizers/AppetizerListCell.swift
@@ -12,8 +12,7 @@ struct AppetizerListCell: View {
     
     var body: some View {
         HStack(spacing: 20) {
-            Image("asian-flank-steak")
-                .resizable()
+            AppetizerRemoteImage(urlString: appetizer.imageURL)
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 120, height: 90)
                 .cornerRadius(8)
