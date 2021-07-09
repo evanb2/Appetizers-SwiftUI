2fdce108ae666bee5d60400634262fbf210073f9
Evan Butler <evanb447@gmail.com>
2021-07-09 10:47:46 -0700

    creates AppetizerListCell view

---
+++ Appetizers/AppetizerListCell.swift
@@ -0,0 +1,37 @@
+//
+//  AppetizerListCell.swift
+//  Appetizers
+//
+//  Created by Evan Butler on 7/9/21.
+//
+
+import SwiftUI
+
+struct AppetizerListCell: View {
+    var appetizer: Appetizer!
+    
+    var body: some View {
+        HStack(spacing: 20) {
+            Image("asian-flank-steak")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 120, height: 90)
+                .cornerRadius(8)
+            VStack(alignment: .leading, spacing: 5) {
+                Text(appetizer.name)
+                    .font(.title2)
+                Text("$\(appetizer.price, specifier: "%.2f")")
+                    .foregroundColor(.secondary)
+                    .fontWeight(.semibold)
+            }
+        }
+    }
+}
+
+struct AppetizerListCell_Previews: PreviewProvider {
+    static var previews: some View {
+        List {
+            AppetizerListCell(appetizer: MockData.sampleAppetizer)
+        }
+    }
+}
